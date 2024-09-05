e3d4933b999ee2533dc3f722d51bf8c0c719edcd
Jad <chehadeh.jad1@gmail.com>
2024-09-05 14:18:08 +0300

    Just Adding the main route link to web browser

--- Zari3atnaDjango/zari3atna/urls.py
+++ Zari3atnaDjango/zari3atna/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('zari3atna/',include('LandingAuth.urls')),
+    path('',include('LandingAuth.urls')),
 ]
