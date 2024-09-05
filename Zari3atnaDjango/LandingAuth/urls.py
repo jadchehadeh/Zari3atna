8b91d3b228b348080d396de07b518d6d92c1b1d1
Jad <chehadeh.jad1@gmail.com>
2024-09-05 16:34:55 +0300

    Testing Database BackEnd and Adding authentication with some links towward it

--- Zari3atnaDjango/LandingAuth/urls.py
+++ Zari3atnaDjango/LandingAuth/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from LandingAuth.views.landing.landing_page import *
+from LandingAuth.views.auth.new_user_auth import * 
 
 urlpatterns = [
     path('', loadLandingPage,name="landing-page"),
-]
+     path('new-user-auth-reg/',new_user_auth,name="user-auth-registration")
+    ]
---
+++ Zari3atnaDjango/LandingAuth/views/auth/new_user_auth.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def new_user_auth(request):
+    return render(request,"landing/authentication.html")
