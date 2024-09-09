b675615c9a679b75fd55afd73f1379457e9aa1fa
Jad <chehadeh.jad1@gmail.com>
2024-09-09 11:45:17 +0300

    This is new update of the apu serializer

--- Zari3atnaBackEnd/zari3atna/user/serializers.py
+++ Zari3atnaBackEnd/zari3atna/user/serializers.py
@@ -13,6 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class UserDetailSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+    photo = serializers.ImageField(required=False)  # Add this line
 
     class Meta:
         model = UserDetail
@@ -23,4 +24,5 @@ class UserDetailSerializer(serializers.ModelSerializer):
         user = UserSerializer.create(UserSerializer(), validated_data=user_data)
         user_detail = UserDetail.objects.create(user=user, **validated_data)
         return user_detail
+
     #Maybe Here I should put update serializer because if i need to add a user profile maybe he need to add a new user#
--- Zari3atnaDjango/LandingAuth/views/auth/new_user_auth.py
+++ Zari3atnaDjango/LandingAuth/views/auth/new_user_auth.py
@@ -6,32 +6,51 @@ def view_user_registration(request):
     return render(request,"landing/authentication.html")
 
 
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+
 def register_user(request):
     if request.method == 'POST':
+        # Extract form data
         username = request.POST.get('userName')
         email = request.POST.get('email')
         first_name = request.POST.get('firstName')
-        last_name  = request.POST.get('lastName')
+        last_name = request.POST.get('lastName')
         password = request.POST.get('passWord')
+        profile_picture = request.FILES.get('profilePicture')
 
+        # Prepare data for POST request
         data = {
-            'username' : username,
-            'email' : email,
-            'first_name' : first_name,
-            'last_name' : last_name,
-            'password' : password
- 
+            'user': {
+                'username': username,
+                'email': email,
+                'first_name': first_name,
+                'last_name': last_name,
+                'password': password
+            },
+            'photo': profile_picture
         }
-        print()
-        response = requests.post('http://127.0.0.1:8001/api33399112222/register-and-detail/', json=data)
-        if response.status_code == 404:
-            return HttpResponse("Api Is Not Correct !")
+
+        # Make POST request to DRF API
+        response = requests.post(
+            'http://127.0.0.1:8001/api33399112222/register-and-detail/',
+            data=data
+        )
+
+        # Handle response
         if response.status_code == 201:
             return HttpResponse("Registration Successful!")
         else:
-            return HttpResponse("Error: Registration failed.")
+            return HttpResponse(f"Error: {response.content.decode()}")
 
-    return render(request, 'authentication.html')
+    return render(request, 'landing/authentication.html')
+
+
 
 
 
